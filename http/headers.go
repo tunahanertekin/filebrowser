a748c9c1746a04e0fd0d7af32e78c1d8f0cf8b9f
tunahanertekin <tunahan.ertekin13@gmail.com>
2024-01-22 17:46:14 +0300

    refactor(install): adapt installation script

--- http/headers.go
+++ http/headers.go
@@ -5,5 +5,9 @@ package http
 
 // global headers to append to every response
 var globalHeaders = map[string]string{
-	"Cache-Control": "no-cache, no-store, must-revalidate",
+	"Cache-Control":                    "no-cache, no-store, must-revalidate",
+	"Access-Control-Allow-Origin":      "*",
+	"Access-Control-Allow-Headers":     "*",
+	"Access-Control-Allow-Methods":     "*",
+	"Access-Control-Allow-Credentials": "true",
 }
